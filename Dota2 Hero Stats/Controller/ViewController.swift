1b40934a4c933f9bbecd7b98306eea4acaf6f1a0
SebaKrk <s.sciuba@gmail.com>
2020-11-02 19:18:21 +0100

    add two override func for NavigationBar

--- Dota2 Hero Stats/Controller/ViewController.swift
+++ Dota2 Hero Stats/Controller/ViewController.swift
@@ -15,8 +15,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func heroStatsButton(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "HeroTable", sender: self)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
     }
     
 }
-
