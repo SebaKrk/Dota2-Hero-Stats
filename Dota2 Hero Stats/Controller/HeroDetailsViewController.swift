09b32e031bd041ce05c875b3d54786b2a6bc1e74
SebaKrk <s.sciuba@gmail.com>
2020-11-02 18:59:55 +0100

    create a new coco touch  files

---
+++ Dota2 Hero Stats/Controller/HeroDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroDetailsViewController.swift
+//  Dota2 Hero Stats
+//
+//  Created by Sebastian Sciuba on 02/11/2020.
+//
+
+import UIKit
+
+class HeroDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
