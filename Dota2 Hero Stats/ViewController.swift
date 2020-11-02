31dda20d894f8bc5ad4f3178c7301b3f4887d4c0
SebaKrk <s.sciuba@gmail.com>
2020-11-02 18:10:03 +0100

    Initial Commit

---
+++ Dota2 Hero Stats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2 Hero Stats
+//
+//  Created by Sebastian Sciuba on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
