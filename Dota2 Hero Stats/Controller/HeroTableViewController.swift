63dfd010a7698e20dbe63530db35c462ccb64368
SebaKrk <s.sciuba@gmail.com>
2020-11-03 08:15:32 +0100

    make a segue to HeroDetailsViewController

--- Dota2 Hero Stats/Controller/HeroTableViewController.swift
+++ Dota2 Hero Stats/Controller/HeroTableViewController.swift
@@ -116,14 +116,16 @@ class HeroTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "DetaliHero", sender: self)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
